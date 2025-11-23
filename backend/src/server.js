e0f51860bb5cba7da621e8824282549179231a45
Dell <paulkurtperocillo@gmail.com>
2025-11-23 15:53:04 +0800

    Add bravo  dependency7

--- backend/src/server.js
+++ backend/src/server.js
@@ -48,7 +48,8 @@ app.use(cors({
   origin: [
     'http://localhost:5173',
     'http://localhost:5174',
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'https://sideline-frontend.onrender.com' // <-- add your deployed frontend here
   ],
   credentials: true
 }));
