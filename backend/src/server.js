f7b6c3bb2e4f5b4d55a2240f2d369d52a81817ff
Dell <paulkurtperocillo@gmail.com>
2025-11-28 16:32:10 +0800

    70

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,6 +13,7 @@ import bcrypt from "bcryptjs";
 import jobsRouter from './api/job.js'; 
 import applicantsRouter from './api/applicants.js';
 import adminRouter from './api/admin.js';
+import { join } from "path";
 
 
 dotenv.config();
@@ -259,6 +260,12 @@ if (user.password.startsWith("$2")) {
   app.use('/api/admin', adminRouter);
 
   app.use("/uploads", express.static(uploadsDir));
+  const frontendPath = join(__dirname, "client/build");
+  app.use(express.static(frontendPath));
+
+  app.get("*", (req, res) => {
+    res.sendFile(join(frontendPath, "index.html"));
+  });
 
   app.listen(port, () => {
     console.log(`🚀 Server running on port ${port}`);
